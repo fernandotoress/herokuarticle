d2625a2cb8e2952924c41a81ede75bd3b43cbfac
fernandotoress <weltonwang88@gmail.com>
2020-01-30 20:09:40 -0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+from newspaper import Article
+from flask import Flask, render_template, request, make_response, g
+
+def particle(request):
+    try:
+        article = Article(request)
+        article.download()
+        article.parse()
+        return article.text
+    except Exception as e:
+        return str(e)
+        
+app = Flask(__name__)
+@app.route("/ping", methods=['POST', 'GET'])
+def ping():
+    return 'pong!'
+    
+@app.route("/run", methods=['POST'])
+def extract():
+    return particle(request.form['url'])
+    
+@app.route("/run", methods=['GET'])
+def extract():
+    return 'Post'
+    
+if __name__ == "__main__":
+
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        
+    )
