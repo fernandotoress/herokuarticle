d409b48f7a60c9875f44615dab8c406589b90ce5
fernandotoress <weltonwang88@gmail.com>
2020-01-30 20:18:26 -0800

    Update server.py

--- server.py
+++ server.py
@@ -23,10 +23,7 @@ def extract():
 def extract():
     return 'Post'
     
-if __name__ == "__main__":
-
-    app.run(
-        host='0.0.0.0',
-        port=8080,
-        
-    )
+app.run(
+    host='0.0.0.0',
+    port=80,
+ )
