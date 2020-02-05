3ad7f2c87e4d33b719d90c04d93304f92222a5f4
fernandotoress <weltonwang88@gmail.com>
2020-02-04 18:06:40 -0800

    Update server.py

--- server.py
+++ server.py
@@ -9,23 +9,23 @@ app = Sanic()
 async def ping(request):
     return text('pong!')
     
-@app.route("/url", methods=['POST'])
+@app.route("/extract-url", methods=['POST'])
 async def extract1(request):
-    data = requests.get(request.body.decode()).text
+    data = requests.get(request.form.get('url')).text
     content = newspaper.fulltext(data)
     return text(content)
 	
-@app.route("/text", methods=['POST'])
+@app.route("/extract-html", methods=['POST'])
 async def extract6(request):
-    return text(newspaper.fulltext(request.body.decode()))
+    return text(newspaper.fulltext(request.form.get('text')))
     
-@app.route("/url", methods=['GET'])
+@app.route("/extract-url", methods=['GET'])
 async def extract(request):
-    return text('Post')
+    return text('API only takes POST requests')
 
-@app.route("/text", methods=['GET'])
+@app.route("/extract-html", methods=['GET'])
 async def extract2(request):
-    return text('Post')
+    return text('API only takes POST requests')
     
     
 if __name__ == '__main__':
